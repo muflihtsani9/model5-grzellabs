8ba2bd8ae128ff5cc59b8ba4393ab1ba352cb378
muflihtsani9 <muflihtsani9@gmail.com>
2026-06-19 22:02:11 +0700

    first commit

--- config/settings.js
+++ config/settings.js
@@ -14,7 +14,7 @@
    1. PIN MASUK
    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
    PIN 4–6 angka yang harus diketik tamu untuk membuka website.  */
-const WEBSITE_PIN = "1410";              // ← GANTI PIN
+const WEBSITE_PIN = "1111";              // ← GANTI PIN
 
 /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
    2. NAMA PENERIMA
